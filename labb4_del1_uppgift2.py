12bd95e5e485f15a53484610d04d1ec93cc9718d
vampiregodofstarlight <113916135+vampiregodofstarlight@users.noreply.github.com>
2022-09-22 12:17:56 +0200

    Update labb4_del1_uppgift2.py

--- labb4_del1_uppgift2.py
+++ labb4_del1_uppgift2.py
@@ -9,7 +9,7 @@ def print_chocolate_bar(M):
     for i in range(len(M)):
         print("\n")
         #After every row, a new line is printed
-        for j in range(len(M[0])):
+        for j in range(len(M[i])):
             print("{:3s}".format(M[i][j]), end="")
             #Along each list, every element M[i][j] is
             #printed, with a minimum width of 3, aligned to the right
