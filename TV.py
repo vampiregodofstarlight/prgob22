24a3f0125bc3c52fa586d1453bf6a24422932291
vampiregodofstarlight <113916135+vampiregodofstarlight@users.noreply.github.com>
2022-10-04 10:24:56 +0200

    Add files via upload

---
+++ TV.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 21 00:16:51 2022
+
+@author: david
+"""
+
+class TV:
+    def __init__(self, tv_name, current_volume, current_channel, max_volume, max_channel):
+        self.tv_name=tv_name
+        self.current_volume=current_volume
+        self.max_volume=max_volume
+        self.current_channel=current_channel
+        self.max_channel=max_channel
+        
+    def change_channel(self, new_channel):
+        if 0<=new_channel<=self.max_channel:
+            self.current_channel = new_channel
+            return True
+        else:
+            return False
+        
+    def increase_volume(self):
+        if 0<=self.current_volume+1<=self.max_volume:
+            self.current_volume+=1
+            return True
+        else:
+            return False
+    
+    def decrease_volume(self):
+        if 0<=self.current_volume-1<=self.max_volume:
+            self.current_volume-=1
+            return True
+        else:
+            return False
+        
+    def __str__(self):
+        return f'{self.tv_name}, channel: {self.current_channel}, volume: {self.current_volume}'
+    
+    def str_for_file(self):
+        return f'{self.tv_name},{self.max_channel},{self.current_channel},{self.max_volume},{self.current_volume}'
+    
+    
+    
+tv = TV('Living room TV', 5,22,10,9)
+tv2 = TV('Bed room TV', 50,7,20,4)
+#print(tv.increase_volume())
+#print(tv)
+#print(tv.increase_volume())
+#print(tv)
+#print(tv.change_channel(3))
+#print(tv)
