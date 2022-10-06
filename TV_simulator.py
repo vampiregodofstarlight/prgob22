e134da21cc760492741c1357836da9d6a1d13ccd
vampiregodofstarlight <113916135+vampiregodofstarlight@users.noreply.github.com>
2022-10-06 16:37:57 +0200

    Update TV_simulator.py

--- TV_simulator.py
+++ TV_simulator.py
@@ -11,15 +11,32 @@ def read_file(tv_file):
     tvfile = open(tv_file, 'r')
     tvlist = []
     for i in tvfile:
-        tvlist.append(i)
+        j = i.strip()
+        tvlist.append(j.split(','))
+        for a in range(len(tvlist)):
+            for b in range(1,len(tvlist[a])):
+                tvlist[a][b]=int(tvlist[a][b])
+                #print(type(tvlist[a][b]))
+                #print((tvlist[a][b]))
+    #TVlist=sum(tvlist,[])
     tvfile.close()
+    #print(tvlist[0])
+    tv=TV(tvlist[0])
     return(tvlist)
         
-print(read_file('allatv.txt'))
+print(read_file('tvfil.txt'))
 
 
-def write_file(TVlist, tv_file):
+def write_file(tvlist, tv_file):
     tvfile = open(tv_file, 'w')
-    for i in TVlist:
-        tvfile.write(i+'\n')
+    for i in range(len(tvlist)):
+        tvfile.write(i)
     tvfile.close()
+    
+    
+    
+tvlist = read_file("tvfil.txt")  
+#for tv in tvlist: print(tv)
+
+#tvlist[0].change_channel(1)
+#for tv in tvlist: print(tv)
