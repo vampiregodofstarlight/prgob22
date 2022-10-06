57cc818d94486383fddcb5d686a5b4daa523be38
vampiregodofstarlight <113916135+vampiregodofstarlight@users.noreply.github.com>
2022-10-07 00:10:42 +0200

    Update TV_simulator.py

--- TV_simulator.py
+++ TV_simulator.py
@@ -10,28 +10,36 @@ from TV import TV
 def read_file(tv_file):
     tvfile = open(tv_file, 'r')
     tvlist = []
+    objlist = []
     for i in tvfile:
         j = i.strip()
         tvlist.append(j.split(','))
         for a in range(len(tvlist)):
             for b in range(1,len(tvlist[a])):
                 tvlist[a][b]=int(tvlist[a][b])
-                #print(type(tvlist[a][b]))
-                #print((tvlist[a][b]))
-    #TVlist=sum(tvlist,[])
+                #tv_obj=TV()
+    #print(tv_obj)
+    for c in range(len(tvlist)):
+        tv_obj=TV(tvlist[c][0],tvlist[c][1],tvlist[c][2],tvlist[c][3],tvlist[c][4])
+        objlist.append(tv_obj)
     tvfile.close()
-    #print(tvlist[0])
-    tv=TV(tvlist[0])
-    return(tvlist)
+    return(objlist)
         
-print(read_file('tvfil.txt'))
+print(read_file('allatv.txt'))
 
 
 def write_file(tvlist, tv_file):
     tvfile = open(tv_file, 'w')
     for i in range(len(tvlist)):
-        tvfile.write(i)
+        tvfile.write(tvlist[i].str_for_file()+'\n')
     tvfile.close()
+    
+
+tvlista = read_file("allatv.txt")
+tvlista[0].change_channel(1)
+tvlista[0].decrease_volume()   
+write_file(tvlista, "allatv.txt")    
+for tv in tvlista: print(tv) 
     
     
     
